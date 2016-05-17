b5bd8314505910af4b0403e4779e586782dea00c
Ele <ecpalmer21@gmail.com>
2016-05-17 15:49:18 -0400

    SendGrid implemented

--- KeepSaving/App_Start/IdentityConfig.cs
+++ KeepSaving/App_Start/IdentityConfig.cs
@@ -11,15 +11,38 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using KeepSaving.Models;
+using System.Configuration;
+using SendGrid;
+using System.Net.Mail;
 
 namespace KeepSaving
 {
     public class EmailService : IIdentityMessageService
     {
-        public Task SendAsync(IdentityMessage message)
+        public async Task SendAsync(IdentityMessage message)
         {
             // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            var apiKey = ConfigurationManager.AppSettings["SendGridAPIKey"];
+            var from = ConfigurationManager.AppSettings["ContactEmail"];
+
+            SendGridMessage myMessage = new SendGridMessage();
+            myMessage.AddTo(message.Destination);
+            myMessage.From = new MailAddress(from);
+            myMessage.Subject = message.Subject;
+            myMessage.Html = message.Body;
+
+            // Create a Web transport, using API Key
+            var transportWeb = new Web(apiKey);
+            // Send the email.
+            try
+            {
+                await transportWeb.DeliverAsync(myMessage);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                await Task.FromResult(0);
+            }
         }
     }
 
