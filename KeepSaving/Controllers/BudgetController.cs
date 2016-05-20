df609e3d2bfb1d2f45c013fbe39e3d385070a14e
Ele <ecpalmer21@gmail.com>
2016-05-20 16:58:25 -0400

    budget scaffolded

---
+++ KeepSaving/Controllers/BudgetController.cs
@@ -0,0 +1,51 @@
+﻿using KeepSaving.Helpers;
+using KeepSaving.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KeepSaving.Controllers
+{
+    public class BudgetController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Budget
+        public ActionResult Index()
+        {
+            var householdId = User.Identity.GetHouseholdId();
+            var household = db.Households.Find(householdId);
+            var model = household.Budget;
+            return View(model);
+        }
+
+        //GET: Budget/_AddBudgetItem
+        public ActionResult _AddBudgetItem()
+        {
+            return PartialView();
+        }
+
+        //POST: Add Budget Item
+        [HttpPost]
+        public ActionResult AddBudgetItem()
+        {
+            return RedirectToAction("Index");
+        }
+
+        //POST: Edit Budget Item
+        [HttpPost]
+        public ActionResult EditBudgetItem()
+        {
+            return RedirectToAction("Index");
+        }
+
+        //POST: Edit Budget Amount
+        [HttpPost]
+        public ActionResult EditBudgetAmount()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
